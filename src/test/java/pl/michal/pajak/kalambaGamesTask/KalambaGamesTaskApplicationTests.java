f2cb1128c3863178d7873b8bc9d53553d525c4d7
spidomisiek88 <michal.pajak88@gmail.com>
2019-05-09 16:29:19 +0200

    first commit

---
+++ src/test/java/pl/michal/pajak/kalambaGamesTask/KalambaGamesTaskApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.michal.pajak.kalambaGamesTask;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KalambaGamesTaskApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
