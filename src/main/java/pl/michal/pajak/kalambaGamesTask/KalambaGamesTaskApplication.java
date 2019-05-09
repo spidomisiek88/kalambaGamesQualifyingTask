104f65038e092a6181ca1015ce0e0f048df5d5cb
spidomisiek88 <michal.pajak88@gmail.com>
2019-05-09 16:38:18 +0200

    application file

---
+++ src/main/java/pl/michal/pajak/kalambaGamesTask/KalambaGamesTaskApplication.java
@@ -0,0 +1,13 @@
+package pl.michal.pajak.kalambaGamesTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalambaGamesTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KalambaGamesTaskApplication.class, args);
+	}
+
+}
